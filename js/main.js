757523999925b6a480536e383ef77b20eda6a86d
alockwood <alockwood@clearvision-cm.com>
2014-03-12 14:01:27 +0000

    got init function to work like in most webgl examples

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
+var scene;
+var camera;
+var renderer;
 
+if( !init() )	animate();
+
+function init(){
 /* three setup */
-var width = window.innerWidth;
-var height = window.innerHeight;
+width = window.innerWidth;
+height = window.innerHeight;
 
-var renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setClearColor( 0xaaccff, 1 ); //
 renderer.setSize(width, height);
 document.body.appendChild(renderer.domElement);
 
-var scene = new THREE.Scene;
+scene = new THREE.Scene;
 
-var cubes;
+
 
 /* Camera Setup */
-var camera = new THREE.PerspectiveCamera(40, width / height, 0.1, 2000);
+camera = new THREE.PerspectiveCamera(40, width / height, 0.1, 2000);
 camera.position.y = 1; //160;
 camera.position.z = 4.22; // do not set later when free move
 //camera.lookAt(new THREE.Vector3(0,0,0));
@@ -25,38 +31,53 @@ camera.matrixWorldInverse.getInverse( camera.matrixWorld );
 scene.add(camera);
 
 /* define a cube */
-var cubeGeometry = new THREE.CubeGeometry(1, 1, 1);
+cubeGeometry = new THREE.CubeGeometry(1, 1, 1);
 
 	/* build add cubes */
-	var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xDDDDDD });
-	//cubeMaterial.color.setRGB (1, 1, 1);
+	cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xDDDDDD });
 	cubes = new THREE.Mesh(cubeGeometry, cubeMaterial);
 	cubes.position.z = 0;
 	cubes.position.x = 0;
 	scene.add(cubes);
 	
-	var level = new THREE.PlaneGeometry( 5, 5 );
-	var material = new THREE.MeshBasicMaterial( {color: 0xfffff0, side: THREE.DoubleSide} );
-	var plane = new THREE.Mesh( level, material );
+	level = new THREE.PlaneGeometry( 5, 5 );
+	material = new THREE.MeshBasicMaterial( {color: 0xfffff0, side: THREE.DoubleSide} );
+	plane = new THREE.Mesh( level, material );
 	scene.add( plane )
 
 /* skybox setup */
-var skyboxGeometry = new THREE.CubeGeometry(10000, 10000, 10000);
-var skyboxMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, side: THREE.BackSide });
-var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
+skyboxGeometry = new THREE.CubeGeometry(10000, 10000, 10000);
+skyboxMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, side: THREE.BackSide });
+skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
 
 scene.add(skybox);
 
 /* Light setup */
-var pointLight = new THREE.PointLight(0xffffff);
+pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(0, 300, 200);
 scene.add(pointLight);
+}
+
+// animation loop
+function animate() {
+
+	// loop on request animation loop
+	// - it has to be at the begining of the function
+	// - see details at http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+	requestAnimationFrame( animate );
+
+	// do the render
+	render();
+
+	// update stats
+	//stats.update();
+}
+
 
 /* animation loop */
 function render() {
 
-	requestAnimationFrame(render);
+	//requestAnimationFrame(render);
 	
 	renderer.render(scene, camera);
 }
-render();
