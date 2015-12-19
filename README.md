e52f1a8af6f47f17cf6fb90aba5a5d65a1568a72
Adam Lockwood <michaeladamlockwood@googlemail.com>
2015-12-19 19:40:09 +0000

    keeping master updated

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 Cube_Hide_N_Seek - server
 ================
 
-https://github.com/PiusNyakoojo/multiplayer_template -- good example
+https://github.com/PiusNyakoojo/multiplayer_template -- base example
 http://blog.csainty.com/2012/01/running-nodejs-server-alongside-your.html - for when we close
 http://socket.io/
+https://github.com/jeromeetienne/threex.cloudgaming - might want for ps4 or not webgl browsers?
+http://www.threejsgames.com/extensions/
 
 This is the main client builder with npm install, it will build the required node packages and dependencies.
 
 grunt build
-With ' grunt '  - It will run a standard instance of our game.
-With ' grunt dev ' - It will build a development version of the game with livereload on changes to src (source) folder.
+grunt - will build and run a serve to develop the webkit page
+grunt build - just build everything
+grunt watch - watch everything
+
+build and run via webkit
+npm start
+
+run not via webkit app:
+cd  into build/serve run:
+node app.js
--- src/serve/js/client_world.js
+++ src/serve/js/client_world.js
@@ -12,16 +12,8 @@ var three = THREE.Bootstrap();
 
 var loadWorld = function(){
 
-    // init();
-    // animate();
-
-    //function init(){
-
-      //var three = THREE.Bootstrap();
-
-      // Insert a cube
-      // var mesh = new THREE.Mesh(new THREE.CubeGeometry(.5, .5, .5), new THREE.MeshNormalMaterial());
-      // three.scene.add(mesh);
+      var random_cube = create_cube();
+      three.scene.add( random_cube );
 
       //add an ambient light
       var ambient = new THREE.AmbientLight( 0x050505 );
@@ -45,13 +37,6 @@ var loadWorld = function(){
       three.camera.position.set(1, 1, 0.5);
 
       three.on('update', function () {
-        // var t = three.Time.now;
-        //
-        //
-        //three.camera.position.set(1, 1, 0.5);
-        // //mesh.rotation.set(Math.sin(t), Math.cos(t), Math.tan(t));
-        // three.camera.lookAt(new THREE.Vector3());
-
         if ( player ){
           updateCameraPosition();
           checkKeyStates();
@@ -60,87 +45,7 @@ var loadWorld = function(){
 
       });
 
-
-
       three.init();
-      /*
-      //Setup------------------------------------------
-      container = document.getElementById('container');
-
-      scene = new THREE.Scene();
-
-      camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
-      camera.position.z = 2;
-      //camera.lookAt( new THREE.Vector3(0,0,0));
-
-      renderer = new THREE.WebGLRenderer( { alpha: false} );
-      renderer.setSize( window.innerWidth, window.innerHeight);
-
-      raycaster = new THREE.Raycaster();
-      //Add Objects To the Scene HERE-------------------
-
-      directionalLight = new THREE.DirectionalLight( 0xffffff, 2 );
-      directionalLight.position.set( 5, 5, 10 ).normalize();
-      var ambient = new THREE.AmbientLight( 0x050505 );
-
-      directionalLight2 = new THREE.DirectionalLight( 0xd1bd92, 2 );
-      directionalLight2.position.set( -5, -5, -10 ).normalize();
-
-      var ranCube = create_cube();
-      place_and_rotate(ranCube);
-      console.log(ranCube);
-
-      var ranCube2 = create_cube();
-      place_and_rotate(ranCube2);
-      console.log(ranCube2);
-
-      //Sphere------------------
-      var sphere_geometry = new THREE.SphereGeometry(1);
-      var sphere_material = new THREE.MeshNormalMaterial();
-      sphere = new THREE.Mesh( sphere_geometry, sphere_material );
-
-      scene.add( directionalLight );
-      scene.add( directionalLight2 );
-      scene.add( ambient );
-
-      scene.add( ranCube );
-      scene.add( ranCube2 );
-
-      scene.add( sphere );
-      objects.push( sphere ); //if you are interested in detecting an intersection with this sphere
-
-      //Events------------------------------------------
-      document.addEventListener('click', onMouseClick, false );
-      document.addEventListener('mousedown', onMouseDown, false);
-      document.addEventListener('mouseup', onMouseUp, false);
-      document.addEventListener('mousemove', onMouseMove, false);
-      document.addEventListener('mouseout', onMouseOut, false);
-      document.addEventListener('keydown', onKeyDown, false );
-      document.addEventListener('keyup', onKeyUp, false );
-      window.addEventListener( 'resize', onWindowResize, false );
-
-      //Final touches-----------------------------------
-      container.appendChild( renderer.domElement );
-      document.body.appendChild( container );
-      */
-    //} // end init
-
-    // function animate(){
-    //   requestAnimationFrame( animate );
-    //   render();
-    // }
-    //
-    // function render(){
-    //
-    //     if ( player ){
-    //       updateCameraPosition();
-    //       checkKeyStates();
-    //       camera.lookAt( player.position );
-    //     }
-    //     //Render Scene---------------------------------------
-    //     renderer.clear();
-    //     renderer.render( scene , camera );
-    // }
 
     function onMouseClick(){
       intersects = calculateIntersects( event );
@@ -229,18 +134,12 @@ var createPlayer = function(data){
     // player = create_cube(data);
     // console.log(player);
 
-    // cube_geometry2 = new THREE.BoxGeometry(data.sizeX, data.sizeX, data.sizeX);
-    // cube_material2 = new THREE.MeshLambertMaterial({color: 0x7777ff});
-    // player = new THREE.Mesh(cube_geometry2, cube_material2);
-
-    player = new THREE.Mesh(new THREE.CubeGeometry(1, 1, 1), new THREE.MeshLambertMaterial({color: 0x7777ff}));
+    player = new THREE.Mesh(new THREE.CubeGeometry(data.sizeX, data.sizeX, data.sizeX), new THREE.MeshLambertMaterial({color: 0x7777ff, transparent:true, opacity:0.8, side: THREE.DoubleSide}));
 
     player.rotation.set(0,0,0);
     player.position.x = data.x;
     player.position.y = data.y;
     player.position.z = data.z;
-
-
 
     playerId = data.playerId;
     moveSpeed = data.speed;
@@ -252,9 +151,9 @@ var createPlayer = function(data){
 };
 
 var updateCameraPosition = function(){
-  three.camera.position.x = player.position.x + 6 * Math.sin( player.rotation.y );
-  three.camera.position.y = player.position.y + 6;
-  three.camera.position.z = player.position.z + 6 * Math.cos( player.rotation.y );
+  three.camera.position.x = player.position.x + 2* Math.sin( player.rotation.y );
+  three.camera.position.y = player.position.y + 2;
+  three.camera.position.z = player.position.z + 2.5 * Math.cos( player.rotation.y );
 };
 
 var updatePlayerPosition = function(data){
@@ -287,15 +186,19 @@ var checkKeyStates = function(){
 
     if (keyState[38] || keyState[87]) {
         // up arrow or 'w' - move forward
-        player.position.x -= moveSpeed * Math.sin(player.rotation.y);
-        player.position.z -= moveSpeed * Math.cos(player.rotation.y);
+        //player.position.x -= moveSpeed * Math.sin(player.rotation.y);
+        //player.position.z -= moveSpeed * Math.cos(player.rotation.y);
+        player.rotation.x += turnSpeed;
+
+
+
         updatePlayerData();
         socket.emit('updatePosition', playerData);
     }
     if (keyState[40] || keyState[83]) {
         // down arrow or 's' - move backward
-        player.position.x += moveSpeed * Math.sin(player.rotation.y);
-        player.position.z += moveSpeed * Math.cos(player.rotation.y);
+        //player.position.x += moveSpeed * Math.sin(player.rotation.y);
+        //player.position.z += moveSpeed * Math.cos(player.rotation.y);
         updatePlayerData();
         socket.emit('updatePosition', playerData);
     }
